ec3fbf5bcc55352d88b388a3bbde023dddafb3cb
Russann Johnson <reachrussann@gmail.com>
2015-09-29 11:11:37 -0600

    latest changes

--- client/js/app.js
+++ client/js/app.js
@@ -10,7 +10,43 @@ angular
   .controller('MainCtrl', function (reps) {
     var main = this;
     main.reps = [];
-    main.sens = [];
+    main.congressType = 'reps';
+
+    main.apis = [
+      {
+        label: 'Zip',
+          method: function (zip){
+            main.loading = true;
+            reps('all', 'zip', zip).then(function(data) {
+              main.reps = data;
+              main.loading = false;
+
+          });
+        }
+      },
+      {
+        label: 'Last Name',
+          method: function (name){
+            main.loading = true;
+            reps(main.congressType, 'name', name).then(function(data){
+              main.reps = data;
+              main.loading = false;
+        });
+      }
+      },
+      {
+          label: 'State',
+          method: function (state){
+            main.loading = true;
+            reps(main.congressType,'state', state).then(function(data){
+              main.reps = data;
+              main.loading = false;
+            });
+          }
+        }
+    ];
+
+    main.criteria = main.apis[0];
 
     main.searchByZip = function (zip) {
       reps.allByZip(zip).then(function (data) {
@@ -44,41 +80,28 @@ angular
   .module('repsService', [])
   .factory('reps', function ($http) {
     var host = 'http://dgm-representatives.herokuapp.com';
-    return {
-      allByZip: function (zip) {
-        return $http
-          .get(host + '/all/by-zip/' + zip)
-          .then(function (response) {
-            return response.data;
-          });
-      },
-      repsByName: function (name) {
-        return $http
-          .get(host + '/reps/by-name/' + name)
-          .then(function (response) {
-            return response.data;
-          });
-      },
-      repsByState: function (state) {
-        return $http
-          .get(host + '/reps/by-state/' + state)
-          .then(function (response) {
-            return response.data;
-          });
-      },
-      sensByName: function (name) {
-        return $http
-          .get(host + '/sens/by-name/' + name)
-          .then(function (response) {
-            return response.data;
-          });
-      },
-      sensByState: function (state) {
-        return $http
-          .get(host + '/sens/by-state/' + state)
-          .then(function (response) {
-            return response.data;
-          });
-      },
-    };
+
+/**
+* @function search
+* @parameter {String } type - can be "all", "reps", "sens"
+* @parameter {String} criteria - can be "zip", "name", "state"
+* @parameter {String} query - can any string
+**/
+function search (type, criteria, query){
+  return $http
+  .get(host + '/' + type + '/by-' + criteria + '/' + query)
+  .then(function (response) {
+    return response.data;
+  });
+    }
+
+
+search.allByZip = search.bind(null, 'all', 'zip');
+search.repsByName = search.bind(null, 'reps', 'name');
+search.repsByState = search.bind(null, 'reps', 'state');
+search.sensByName = search.bind(null, 'sens', 'name');
+search.sensbyState = search.bind(null, 'sens', 'state');
+
+
+    return search;
   });
